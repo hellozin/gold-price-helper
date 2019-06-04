dd296fab9b761068b05e096f46ea997c717d4ece
hellozin <paul26375@gmail.com>
2019-06-04 23:13:45 +0900

    Initial commit

---
+++ src/main/java/me/hellozin/goldpricehelper/GoldPriceHelperApplication.java
@@ -0,0 +1,13 @@
+package me.hellozin.goldpricehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldPriceHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldPriceHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/hellozin/goldpricehelper/GoldPriceHelperApplicationTests.java
@@ -0,0 +1,16 @@
+package me.hellozin.goldpricehelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoldPriceHelperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
